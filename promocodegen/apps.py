9ab8e6afd366397a5cafa742ead365d475524d76
FadeevDeniss <deniro094@gmail.com>
2022-04-21 17:17:20 +0300

    Create db model for storing promocode data, generatepromo app

---
+++ promocodegen/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PromocodegenConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'promocodegen'
---
+++ promocodegen/models.py
@@ -0,0 +1,12 @@
+from django.db.models import Model, CharField, DateTimeField
+
+
+class PromoCodes(Model):
+
+    code = CharField(max_length=12, blank=True, null=True, unique=True)
+    group = CharField(max_length=20, null=False)
+
+    created_at = DateTimeField(auto_now=True, null=False)
+
+    def __str__(self):
+        return f"group: {self.group}, created at: {self.created_at}"
